65bb6c904ba1590205b11e245727a3c8a5d56c03
Christopher Ksiazak <cksiazak@gmail.com>
2019-08-19 19:03:52 -0600

    used codemod to rename unsafe lifecycles

--- src/Components/navigation/navigation.js
+++ src/Components/navigation/navigation.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import * as Scroll from 'react-scroll';
 
 import './navigation.scss';
 
-const FadeExample = () => {
+const Navigation = () => {
   return (
     <div>
       <ul className="test">
-        <Fade top delay={200}>
-          <h1>React Reveal</h1>
-        </Fade>
         <Fade top delay={400}>
           <h1>React Reveal</h1>
         </Fade>
@@ -19,9 +17,12 @@ const FadeExample = () => {
         <Fade top delay={800}>
           <h1>React Reveal</h1>
         </Fade>
+        <Fade top delay={1000}>
+          <h1>React Reveal</h1>
+        </Fade>
       </ul>
     </div>
   );
 };
 
-export default FadeExample;
+export default Navigation;
