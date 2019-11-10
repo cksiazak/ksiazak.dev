e7157cbcc638b84e3242651625bb46687ff7283b
Christopher Ksiazak <cksiazak@gmail.com>
2019-11-10 01:47:46 -0700

    added gatsby module for reveal

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-scroll-reveal`,
+      options: {
+        threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
+        once: true, // Defines if animation needs to be launched once
+        selector: '[data-reveal]'
+      }
+    }
   ]
 };
