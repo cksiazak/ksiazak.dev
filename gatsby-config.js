b31b2e0e71556c17f5b2db16526efd625fd717c6
Christopher Ksiazak <cksiazak@gmail.com>
2019-11-18 00:46:12 -0700

    various testings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "Christopher Ksiazak",
-    siteUrl: "https://www.ksiazak.dev",
-    description: "Personal portolio for Christopher Ksiazak"
+    title: 'Christopher Ksiazak',
+    siteUrl: 'https://www.ksiazak.dev',
+    description: 'Personal portolio for Christopher Ksiazak'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-robots-txt`,
     {
-      resolve: `gatsby-plugin-scroll-reveal`,
+      resolve: 'gatsby-plugin-html-attributes',
       options: {
-        threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        selector: "[data-reveal]"
-      }
-    },
-    {
-      resolve: "gatsby-plugin-html-attributes",
-      options: {
-        lang: "en"
+        lang: 'en'
       }
     }
   ]
