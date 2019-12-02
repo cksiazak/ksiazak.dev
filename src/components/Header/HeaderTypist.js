40c50158a7f679c2120611c5060e53c31c6039b2
Christopher Ksiazak <cksiazak@gmail.com>
2019-12-01 22:08:35 -0700

    debugging

--- src/components/Header/HeaderTypist.js
+++ src/components/Header/HeaderTypist.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Typist from 'react-typist';
+import React, { Fragment } from 'react';
+// import Typist from 'react-typist';
 
 const HeaderTypist = () => {
   return (
-    <Typist cursor={{ show: false }} startDelay={1200} avgTypingDelay={55}>
-      <span className="initial-hello">
-        Hello, <Typist.Delay ms={250} /> my name is
-      </span>
+    <Fragment>
+      {/* <Typist cursor={{ show: false }} startDelay={1200} avgTypingDelay={55}> */}
+      <span className="initial-hello">Hello, my name is</span>
       <h1 className="header-name">Christopher Ksiazak</h1>
-      <Typist.Delay ms={250} />
+      {/* <Typist.Delay ms={250} /> */}
       <span className="short-bio">& I like to build things.</span>
-    </Typist>
+      {/* </Typist> */}
+    </Fragment>
   );
 };
 
