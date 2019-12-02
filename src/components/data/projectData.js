185244f475ad21e0e2ce41e09cb96901489a9857
Christopher Ksiazak <cksiazak@gmail.com>
2019-12-01 21:29:57 -0700

    added project descriptions

--- src/components/data/projectData.js
+++ src/components/data/projectData.js
@@ -6,27 +6,41 @@ export const projects = [
     description: [
       "This is my portfolio page (the one that you're currently on.) The front end is built using Gatsby.js, which pulls from my personal api that is handles by Express. You may send a GET request to https://api.ksiazak.dev for the JSON output."
     ],
-    technologies: ['Gatsby.js', 'Node/Express.js'],
+    technologies: ['Gatsby.js', 'Node/Express.js', '(S)CSS'],
     imgurl: ''
   },
   {
-    title: 'Ksiazak.dev',
-    url: 'https://ksiazak.dev',
-    githubLink: 'https://github.com/cksiazak/ksiazak.dev',
+    title: 'SongSight',
+    url: 'https://song-suggester-bw.netlify.com/',
+    githubLink:
+      'https://github.com/Build-Week-Spotify-Song-Suggester-PT/front-end-react',
     description: [
-      "This is my portfolio page (the one that you're currently on.) The front end is built using Gatsby.js, which pulls from my personal api that is handles by Express. You may send a GET request to https://api.ksiazak.dev for the JSON output."
+      'SongSight allows you to select from a list of songs, save that song to your list, or select similar songs based on danceability, key, bpm, and other metrics.',
+      'This was part 3 of our build week projcts at Lambda school. I worked with 3 other React developers in planning out the application layout and function, as well as with our back-end developer and data science team.'
     ],
-    technologies: ['Gatsby.js', 'Node/Express.js'],
+    technologies: ['React.js', 'React-Router', 'React-Redux', 'Grommet'],
     imgurl: ''
   },
   {
-    title: 'Ksiazak.dev',
-    url: 'https://ksiazak.dev',
-    githubLink: 'https://github.com/cksiazak/ksiazak.dev',
+    title: 'SIXR',
+    url: 'https://sixrfund.netlify.com/',
+    githubLink: 'https://github.com/vr-funding-platform/frontend',
     description: [
-      "This is my portfolio page (the one that you're currently on.) The front end is built using Gatsby.js, which pulls from my personal api that is handles by Express. You may send a GET request to https://api.ksiazak.dev for the JSON output."
+      'SIXR is a project creation and investment piece. This allows you to either find a project to invest in, or create a new project for others to select from.',
+      "I was paired up with another React developer, as well as a UI designer. I created the login, sign up, and project list pages to the (near) pixel perfect design our designer had for us. Full functionality isn't complete yet, but I am planning on a re-design."
     ],
-    technologies: ['Gatsby.js', 'Node/Express.js'],
+    technologies: ['React.js', 'React-Router', '(S)CSS'],
+    imgurl: ''
+  },
+  {
+    title: 'DevDesk',
+    url: 'https://pt7-devdesk-queue.github.io/devdesk-queue/index.html',
+    githubLink: 'https://github.com/pt7-devdesk-queue/devdesk-queue',
+    description: [
+      'This is the marketing page for DevDesk. This was our first build week projects utilizing HTML, CSS (with LESS), and vanilla Javascript.',
+      'I worked with another front-end developer, and I created the Home and Solutions pages. This was the first build project.'
+    ],
+    technologies: ['HTML', 'Javascript', 'CSS (LESS)'],
     imgurl: ''
   }
 ];
