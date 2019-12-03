72645fef39748735cf2cece3c8ca60ac97e99935
Christopher Ksiazak <cksiazak@gmail.com>
2019-12-02 22:34:42 -0700

    added same data as api

--- src/components/data/projectData.js
+++ src/components/data/projectData.js
@@ -4,7 +4,7 @@ export const projects = [
     url: 'https://ksiazak.dev',
     githubLink: 'https://github.com/cksiazak/ksiazak.dev',
     description: [
-      "This is my portfolio page (the one that you're currently on.) The front end is built using Gatsby.js, which pulls from my personal api that is handled by Node/Express.js.",
+      "This is my portfolio page (the one that you're currently on.) The front end is built using Gatsby.js. I also created a quick api using Node/Express.js using the same information.",
       'You may send a GET request to https://api.ksiazak.dev for the JSON output.'
     ],
     technologies: ['Gatsby.js', 'Node/Express.js', '(S)CSS'],
