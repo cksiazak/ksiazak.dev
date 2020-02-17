9c41496975cfd780732b22eacb138b332daf048a
Christopher Ksiazak <cksiazak@gmail.com>
2020-02-17 13:32:23 -0700

    restarting fresh

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,6 @@
 import React, { Fragment } from 'react';
-import styled from 'styled-components';
-import Head from 'next/head';
 
-// Components
-import Navigation from '../components/Navigation/Navigation';
-import Header from '../components/Header/Header';
-import About from '../components/About/About';
-
-const AppWrapper = styled.div`
-  width: 70%;
-  margin: 0 auto;
-`;
-
-const Home = () => (
-  <Fragment>
-    <Head>
-      <meta charSet='utf-8' />
-      <title>Christopher Ksiazak</title>
-      <meta
-        name='description'
-        content='A personal developer portfolio and art piece by Christopher Ksiazak'
-      />
-      <meta
-        name='keywords'
-        content='Christopher Ksiazak, Chris Ksiazak, Ksiazak, developer, web, frontend, backend, react, engineer, nodejs, javascript'
-      />
-    </Head>
-    <Navigation />
-    <AppWrapper>
-      <Header />
-      <About />
-    </AppWrapper>
-  </Fragment>
-);
-
+const Home = () => {
+  return <div>TEST</div>;
+};
 export default Home;
--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -1,8 +1,8 @@
-import styled, { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
-  ${reset};
+  ${normalize};
 
   * {
     box-sizing: border-box;
