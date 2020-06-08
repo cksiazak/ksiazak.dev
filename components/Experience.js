a975ee7eb9a8f13a92f89900f16aa6803d9ab2ef
Christopher Ksiazak <cksiazak@gmail.com>
2020-06-08 01:06:28 -0600

    updated

--- components/Experience.js
+++ components/Experience.js
@@ -1,16 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 
 // stylings
-import styled from 'styled-components';
-import { theme } from '../constants/themes';
+import styled from 'styled-components'
+import { theme } from '../constants/themes'
 
 // components
-import ExperienceCard from './Experience/ExperienceCard';
+import ExperienceCard from './Experience/ExperienceCard'
 
 // data
-import * as EDealer from '../data/experiences/e-dealer-direct.mdx';
-import * as C3i from '../data/experiences/c3i-solutions.mdx';
-import ThemeContext from '../lib/ThemeContext';
+import * as PatrickThomas from '../data/experiences/patrick-thomas.mdx'
+import * as EDealer from '../data/experiences/e-dealer-direct.mdx'
+import * as C3i from '../data/experiences/c3i-solutions.mdx'
+import ThemeContext from '../lib/ThemeContext'
 
 const WorkSection = styled.section`
   padding: 75px 0px 30px;
@@ -20,7 +21,7 @@ const WorkSection = styled.section`
   @media (max-width: 900px) {
     padding: 50px 0px 30px;
   }
-`;
+`
 
 const SectionHeading = styled.h2`
   font-size: 3.75rem;
@@ -28,7 +29,7 @@ const SectionHeading = styled.h2`
   @media (max-width: 900px) {
     margin: 0px;
   }
-`;
+`
 
 const InnerWrap = styled.div`
   width: ${theme.global.pageWidth};
@@ -38,19 +39,20 @@ const InnerWrap = styled.div`
   @media (max-width: 600px) {
     width: 90%;
   }
-`;
+`
 
 const Experience = () => {
-  const { darkMode } = useContext(ThemeContext);
+  const { darkMode } = useContext(ThemeContext)
   return (
     <WorkSection>
       <SectionHeading darkMode={darkMode}>Experiences</SectionHeading>
       <InnerWrap>
+        <ExperienceCard work={PatrickThomas} />
         <ExperienceCard work={EDealer} />
         <ExperienceCard work={C3i} />
       </InnerWrap>
     </WorkSection>
-  );
-};
+  )
+}
 
-export default Experience;
+export default Experience
--- components/Projects.js
+++ components/Projects.js
@@ -1,18 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 
 // themes
-import styled from 'styled-components';
-import { theme } from '../constants/themes';
-import ThemeContext from '../lib/ThemeContext';
+import styled from 'styled-components'
+import { theme } from '../constants/themes'
+import ThemeContext from '../lib/ThemeContext'
 
 // Components
-import ProjectCard from './Projects/ProjectCard';
+import ProjectCard from './Projects/ProjectCard'
 
 // Markdown Data
-import * as SwagDragon from '../data/projects/swag-dragon.mdx';
-import * as Ksiazakdev from '../data/projects/ksiazak-dev.mdx';
-import * as AirBnBPricing from '../data/projects/airbnb-optimal-pricing.mdx';
-import * as SongSight from '../data/projects/song-sight.mdx';
+import * as Ksiazakdev from '../data/projects/ksiazak-dev.mdx'
+import * as AirBnBPricing from '../data/projects/airbnb-optimal-pricing.mdx'
+import * as SongSight from '../data/projects/song-sight.mdx'
 
 const ProjectSection = styled.section`
   display: flex;
@@ -24,7 +23,7 @@ const ProjectSection = styled.section`
   @media (max-width: 900px) {
     padding-top: 50px;
   }
-`;
+`
 
 const SectionHeader = styled.h2`
   font-size: 3.75rem;
@@ -34,7 +33,7 @@ const SectionHeader = styled.h2`
     margin-top: 0px;
     margin-bottom: 10px;
   }
-`;
+`
 
 const InnerProjectWrapper = styled.div`
   width: ${theme.global.pageWidth};
@@ -45,21 +44,20 @@ const InnerProjectWrapper = styled.div`
   @media (max-width: 1200px) {
     width: 90%;
   }
-`;
+`
 
 const Projects = () => {
-  const { darkMode } = useContext(ThemeContext);
+  const { darkMode } = useContext(ThemeContext)
   return (
     <ProjectSection>
       <SectionHeader darkMode={darkMode}>Things I've Built</SectionHeader>
       <InnerProjectWrapper>
-        <ProjectCard project={SwagDragon} />
         <ProjectCard project={Ksiazakdev} />
         <ProjectCard project={AirBnBPricing} />
         <ProjectCard project={SongSight} />
       </InnerProjectWrapper>
     </ProjectSection>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
